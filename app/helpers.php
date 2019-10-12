32a58eaa5919184a77df982fcf4e79f9787875e0
ebychoco <brouyaoeric7@gmail.com>
2019-10-12 19:18:55 +0000

    Configure app name

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,7 @@
 <?php
 if (!function_exists('page_title')){
     function page_title($title){
-        $base_title=' laracarte - List of artisans';
+        $base_title=config('app.name').'- List of artisans';
         if ($title ===''){
             return $base_title;
         }else{
--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="text-center">
-   <p> &copy;{{date('Y')}} &middot; Laracarte by <a href="https://twitter.com/yao">@yao</a> &dash; A <a href="https://Laramap.com">Laramap</a>  clone app.</p>
+   <p> &copy;{{date('Y')}} &middot; {{config('app.name')}} by <a href="https://twitter.com/yao">@yao</a> &dash; A <a href="https://Laramap.com">Laramap</a>  clone app.</p>
    <p><strong>This app has been built for learning purposes.</strong></p>
 </footer>
