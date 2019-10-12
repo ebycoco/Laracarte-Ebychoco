e7d1ba15e7a924c81377da83535a2a0cd9e378d9
ebychoco <brouyaoeric7@gmail.com>
2019-10-12 15:33:08 +0000

    Setup and add static pages

---
+++ resources/views/layouts/partials/_nav.blade.php
@@ -0,0 +1,42 @@
+
+
+<nav class="navbar navbar-default">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{route('home_path')}}">Laracarte</a>
+          </div>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="{{route('home_path')}}">Home</a></li>
+              <li><a href="{{route('about_path')}}">About</a></li>
+              <li><a href="#">Artisans</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="https://laravel.com">Laravel.com</a></li>
+                  <li><a href="https://laravel.io">Laravel.io</a></a></li>
+                  <li><a href="https://Laracast.com">Laracast</a></li>
+                  <li><a href="https://Larajobs.com">Larajobs</a></li>
+                  <li><a href="https://laravel-news.com">Laravel News</a></li>
+                  <li><a href="https://Larachat.com">Larachat</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+            <ul class="nav  navbar-nav navbar-right">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Registrer</a>
+                </li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="container">
+        <h2>What is Laracarte?</h2>
+        <p>Laracarte is a clone app of <a href="https://Laramap.com" target="_blank">Laramap.com</a>.</p>
+        <div class="row">
+            <div class="col-md-6">
+                <p class="alert alert-warning">
+                    <strong><i class="fa fa-exclamation-triangle"></i>This app has been built by<a href="https://twitter.com/yao">@yao</a> for learning purposes.</strong>
+                </p>
+            </div>
+        </div>
+        <p>Feel free to helpto improve the <a href="#">souce code</a>.</p>
+        <hr>
+        <h2>what is Laramap?</h2>
+        <p>Laramap is the website by which Laracarte was inspire :).</p>
+        <p>More info<a href="https://Laramap.com/p/about" target="_blank"> here</a>..</p>
+        <hr>
+        <h2>which tools and services are used in Laracarte?</h2>
+        <p>Basically it's built on laravel &amp; Bootstrap. But there's a bunch of services used for email and other sections.</p>
+        <ul>
+            <li>Laravel</li>
+            <li>Bootstrap</li>
+            <li>Amazone S3</li>
+            <li>Mandril</li>
+            <li>Google Maps</li>
+            <li>Gravatar</li>
+            <li>Antony Martin's Geolocation Package</li>
+            <li>Michel fortin's Markdown Parser Package</li>
+            <li>Heroku</li>
+        </ul>
+    </div>
+@stop
